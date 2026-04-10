26d6d5322d72cdea25824c15f85fbed1aa52e04f
yeseul <joyri0202@gmail.com>
2026-04-10 20:12:55 +0900

    feat(#2): 디자인 시스템 설계 및 공통 컴포넌트 구현

--- app/src/main/java/com/example/letssopt/screen/LoginActivity.kt
+++ app/src/main/java/com/example/letssopt/screen/LoginActivity.kt
@@ -1,27 +1,34 @@
-package com.example.letssopt
+package com.example.letssopt.screen
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.letssopt.ui.theme.LETSSOPTTheme
+import androidx.compose.ui.unit.dp
+import androidx.compose.material3.MaterialTheme.typography
+import androidx.compose.ui.res.stringResource
+import com.example.letssopt.R
+import com.example.letssopt.designsystem.theme.LETSSOPTTheme
+import com.example.letssopt.designsystem.theme.PrimaryRed
 
-class MainActivity : ComponentActivity() {
+class LoginActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             LETSSOPTTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    Login(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +38,27 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun Login(modifier: Modifier = Modifier) {
+    Column(
         modifier = modifier
-    )
+            .fillMaxSize()
+            .padding(10.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
+    ) {
+        Text(
+            text = stringResource(R.string.logo),
+            color = PrimaryRed,
+            style = typography.displayLarge
+        )
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun GreetingPreview2() {
     LETSSOPTTheme {
-        Greeting("Android")
+        Login()
     }
 }
