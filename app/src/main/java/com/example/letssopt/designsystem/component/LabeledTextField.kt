bc5ab1d56882bd6f2b5b348c19a57f4c52de0d92
yeseul <joyri0202@gmail.com>
2026-04-10 23:11:37 +0900

    refactor(#2): 패키지 구조 변경

--- app/src/main/java/com/example/letssopt/designsystem/component/LabeledTextField.kt
+++ app/src/main/java/com/example/letssopt/designsystem/component/LabeledTextField.kt
@@ -1,4 +1,4 @@
-package com.example.letssopt.designsystem.component.button
+package com.example.letssopt.designsystem.component
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
