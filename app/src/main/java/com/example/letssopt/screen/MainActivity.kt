3a0150d1c8bb8dece5cd4506699bebb857b9d14a
yeseul <joyri0202@gmail.com>
2026-04-24 20:07:07 +0900

    feat(#4): BottomNavigationBar 구현

--- app/src/main/java/com/example/letssopt/screen/MainActivity.kt
+++ app/src/main/java/com/example/letssopt/screen/MainActivity.kt
@@ -10,21 +10,25 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.material3.MaterialTheme.typography
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.letssopt.R
-import com.example.letssopt.designsystem.theme.Background
+import com.example.letssopt.designsystem.component.BottomNavigationBar
 import com.example.letssopt.designsystem.theme.LETSSOPTTheme
 import com.example.letssopt.designsystem.theme.PrimaryRed
 import com.example.letssopt.designsystem.theme.TextPrimary
+import com.example.letssopt.navigation.NavigationType
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,13 +36,28 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             LETSSOPTTheme {
+                val navController = rememberNavController()
+
                 Scaffold(
                     modifier = Modifier.fillMaxSize(),
-                    containerColor = MaterialTheme.colorScheme.background
+                    containerColor = MaterialTheme.colorScheme.background,
+                    bottomBar = {
+                        BottomNavigationBar(navController = navController)
+                    }
                 ) { innerPadding ->
-                    Greeting(
+                    NavHost(
+                        navController = navController,
+                        startDestination = NavigationType.HOME,
                         modifier = Modifier.padding(innerPadding)
-                    )
+                    ) {
+                        composable(NavigationType.HOME) {
+                            Greeting()
+                        }
+                        composable(NavigationType.STORE) { Text("Store Screen", color = Color.White) }
+                        composable(NavigationType.WEBTOON) { Text("Webtoon Screen", color = Color.White) }
+                        composable(NavigationType.SEARCH) { Text("Search Screen", color = Color.White) }
+                        composable(NavigationType.LIBRARY) { Text("Library Screen", color = Color.White) }
+                    }
                 }
             }
         }
@@ -72,14 +91,20 @@ fun Greeting(modifier: Modifier = Modifier) {
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
-private fun GreetingPreview() {
+private fun MainFullPreview() {
     LETSSOPTTheme {
-        Surface(
-            color = Background
-        ) {
-            Greeting()
+        val navController = rememberNavController()
+        Scaffold(
+            modifier = Modifier.fillMaxSize(),
+            bottomBar = {
+                BottomNavigationBar(navController = navController)
+            }
+        ) { innerPadding ->
+            Column(modifier = Modifier.padding(innerPadding)) {
+                Greeting()
+            }
         }
     }
 }
